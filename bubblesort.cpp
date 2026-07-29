1ec72574c1576a65335ad8ff29ca26aac6edd6d1
saipraveensaila324 <saipraveen.saila@gmail.com>
2026-07-29 17:18:40 +0530

    Add bubble sort implementation in bubblesort.cpp
    Implement bubble sort algorithm to sort an array.

---
+++ bubblesort.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = 5;
+    int i, j, temp;
+
+    // Bubble Sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
