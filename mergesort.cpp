5de29b7eaccf897ade6ea3c8ca19e06b00b88511
saipraveensaila324 <saipraveen.saila@gmail.com>
2026-07-29 17:50:12 +0530

    Create mergesort.cpp

---
+++ mergesort.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void merge(int arr[], int low, int mid, int high) {
+    int temp[100];
+    int i = low, j = mid + 1, k = low;
+
+    while (i <= mid && j <= high) {
+        if (arr[i] < arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= high)
+        temp[k++] = arr[j++];
+
+    for (i = low; i <= high; i++)
+        arr[i] = temp[i];
+}
+
+void mergeSort(int arr[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+
